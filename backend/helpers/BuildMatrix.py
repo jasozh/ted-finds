3038469433cc05652cbe78190c5069ca4c92a0fb
jk2298 <jk2298@cornell.edu>
2024-03-22 16:22:53 -0400

    finished (not tested) build similarity matrix

--- backend/helpers/BuildMatrix.py
+++ backend/helpers/BuildMatrix.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import re
+import json
 
 def tokenize(text):
     text = text.lower()
@@ -24,11 +25,9 @@ def create_ranked_good_types(
         words_combined += words
 
     tup_list = []
-    
-    good_words = np.array(input_good_types)
     total_len = len(words_combined)
     
-    for word in good_words:
+    for word in input_good_types:
         word_freq = round(words_combined.count(word) / total_len, 5)
         tup_list.append((word, word_freq))
     sorted_tups = sorted(tup_list, key=lambda x : x[1], reverse=True)
@@ -40,37 +39,50 @@ def create_word_occurrence_matrix(
     input_transcripts,
     input_good_types):
     
-    speaker_indices = {speaker: idx for idx, speaker in enumerate(input_transcripts)}
+    speaker_indices = {speaker[2]: idx for idx, speaker in enumerate(input_transcripts)}
     freqs = np.zeros((len(input_transcripts), len(input_good_types)))
 
     word_idx = {}
     for idx, (word, freq) in enumerate(input_good_types):
         word_idx[idx] = word
 
-    for idx, transcript in input_transcripts:
+    for idx, transcript, _ in input_transcripts:
         tokens = tokenize_method(transcript)
         arr = np.zeros(len(input_good_types))
         for word_index in word_idx:
-            good_word, _ = word_idx[word_index]
+            good_word = word_idx[word_index]
             arr[word_index] = tokens.count(good_word)
         freqs[idx] += arr[:]
 
-    return freqs
+    return speaker_indices, freqs
 
 def df_to_word_occ_mat(df):
     list_of_tuples = []
 
     for idx, row in df.iterrows():
-        list_of_tuples.append((idx, row['transcript']))
+        list_of_tuples.append((idx, row['transcript'], row['title']))
 
     counts = types_counts(list_of_tuples)
 
     filtered_counts = {}
     for word in counts:
-        if (counts[word] > 200):
+        if (counts[word] > 100):
             filtered_counts[word] = counts[word]
 
     good_types = create_ranked_good_types(tokenize, tokenize_transcript, list_of_tuples, filtered_counts)
     docname_to_idx, word_occ_mat = create_word_occurrence_matrix(tokenize, list_of_tuples, good_types)
 
     return docname_to_idx, word_occ_mat
+
+def load_data(file_path: str) -> pd.DataFrame:
+    """
+    Loads the TED Talk transcript data as a Pandas dataframe
+    """
+    with open('backend/init.json', 'r') as json_file:
+        data = json.load(json_file)
+        df = pd.json_normalize(data)
+        df = df.head(200)
+    docname_to_idx, word_occ_mat = df_to_word_occ_mat(df)
+    return docname_to_idx, word_occ_mat
+
+docname_to_idx, word_occ_mat = load_data("")
