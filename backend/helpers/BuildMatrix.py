f9eea56500d662ad5dd3436a6118eba20bc35bc2
jk2298 <jk2298@cornell.edu>
2024-03-25 17:20:41 -0400

    continued working on cos sim

--- backend/helpers/BuildMatrix.py
+++ backend/helpers/BuildMatrix.py
@@ -11,10 +11,12 @@ def tokenize_transcript(tokenize_method, transcript):
     return tokenize_method(transcript)
 
 def types_counts(transcripts):
+    print("Started type counts")
     tokens = {}
     for transcript in transcripts:
         for token in tokenize_transcript(tokenize, transcript[1]):
             tokens[token] = tokens.get(token, 0) + 1
+    print("Done Type Counts")
     return tokens
 
 def create_ranked_good_types(
@@ -26,18 +28,24 @@ def create_ranked_good_types(
 
     tup_list = []
     total_len = len(words_combined)
+
+    print("Started ranking")
     
     for word in input_good_types:
         word_freq = round(words_combined.count(word) / total_len, 5)
         tup_list.append((word, word_freq))
-    sorted_tups = sorted(tup_list, key=lambda x : x[1], reverse=True)
+    #sorted_tups = sorted(tup_list, key=lambda x : x[1], reverse=True)
+        
+    print("Done rankings")
     
-    return sorted_tups
+    return tup_list#sorted_tups
 
 def create_word_occurrence_matrix(
     tokenize_method,
     input_transcripts,
     input_good_types):
+
+    print("Creating WOC mat")
     
     speaker_indices = {speaker[2]: idx for idx, speaker in enumerate(input_transcripts)}
     freqs = np.zeros((len(input_transcripts), len(input_good_types)))
@@ -53,6 +61,7 @@ def create_word_occurrence_matrix(
             good_word = word_idx[word_index]
             arr[word_index] = tokens.count(good_word)
         freqs[idx] += arr[:]
+        print(f"Done {idx}")
 
     return speaker_indices, freqs
 
@@ -66,7 +75,7 @@ def df_to_word_occ_mat(df):
 
     filtered_counts = {}
     for word in counts:
-        if (counts[word] > 100):
+        if (counts[word] < 5000):
             filtered_counts[word] = counts[word]
 
     good_types = create_ranked_good_types(tokenize, tokenize_transcript, list_of_tuples, filtered_counts)
@@ -74,15 +83,84 @@ def df_to_word_occ_mat(df):
 
     return docname_to_idx, word_occ_mat
 
-def load_data(file_path: str) -> pd.DataFrame:
+def get_sim(talk1, talk2, input_doc_mat, input_movie_name_to_index):
+
+    idx1 = input_movie_name_to_index[talk1]
+    idx2 = input_movie_name_to_index[talk2]
+
+    m1 = input_doc_mat[idx1]
+    m2 = input_doc_mat[idx2]
+
+    num = np.dot(m1, m2)
+
+    denom = np.linalg.norm(m1) * np.linalg.norm(m2)
+
+    return num/denom
+
+def build_ted_talks_sims_cos(n, movie_index_to_name, input_doc_mat, movie_name_to_index):
+
+    ret_mat = np.zeros((n, n))
+
+    for i in range(n):
+      t1 = movie_index_to_name[i]
+      for j in range(i+1):
+        if i == j:
+          ret_mat[i, j] = 1
+        else:
+          t2 = movie_index_to_name[j]
+          score = get_sim(t1, t2, input_doc_mat, movie_name_to_index)
+          ret_mat[i, j] = ret_mat[j, i] = score
+
+    return ret_mat
+
+def get_rankings(talk, matrix, index, inv):
+    # Get movie index from movie name
+    mov_idx = index[talk]
+
+    # Get list of similarity scores for movie
+    score_lst = matrix[mov_idx]
+    cos_sim_mat = [(inv[i], s) for i,s in enumerate(score_lst)]
+
+    # Do not account for movie itself in ranking
+    cos_sim_mat = cos_sim_mat[:mov_idx] + cos_sim_mat[mov_idx+1:]
+
+    # Sort rankings by score
+    cos_sim_mat = sorted(cos_sim_mat, key=lambda x: -x[1])
+
+    return cos_sim_mat
+
+def invert_dict(d):
+    inverted_dict = {}
+    for item in d:
+        inverted_dict[d[item]] = item
+    return inverted_dict
+
+def load_data():
     """
     Loads the TED Talk transcript data as a Pandas dataframe
     """
-    with open('backend/init.json', 'r') as json_file:
+    with open('../init.json', 'r') as json_file:
         data = json.load(json_file)
         df = pd.json_normalize(data)
-        df = df.head(200)
+        #df = df.head(10)
     docname_to_idx, word_occ_mat = df_to_word_occ_mat(df)
     return docname_to_idx, word_occ_mat
 
-docname_to_idx, word_occ_mat = load_data("")
+def build_similarity_matrix():
+    docname_to_idx, word_occ_mat = load_data()
+    print("Done Loading Data")
+    inv = invert_dict(docname_to_idx)
+    matrix = build_ted_talks_sims_cos(
+        n=len(word_occ_mat), 
+        movie_index_to_name=inv, 
+        input_doc_mat=word_occ_mat, 
+        movie_name_to_index=docname_to_idx)
+    print("Done Building Matrix")
+    #print(get_rankings("Meet the founder of the blog revolution", matrix, docname_to_idx, inv))
+    np.save("cosine_similarity_matrix", matrix)
+    with open("idx_to_docnames", 'w') as json_file:
+        json.dump(inv, json_file)
+    with open("docname_to_idx", 'w') as json_file:
+        json.dump(docname_to_idx, json_file)
+
+    
