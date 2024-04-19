b1f84add2adca96118368c12a7777f9ffb9c0d14
jk2298 <jk2298@cornell.edu>
2024-04-19 16:32:16 -0400

    started working on categories

--- backend/helpers/BuildMatrix.py
+++ backend/helpers/BuildMatrix.py
@@ -15,8 +15,81 @@ def build_sim_matrix(documents, k=100):
     # u, s, v_trans = svds(td_matrix, k=k)
 
     docs_compressed, s, words_compressed = svds(td_matrix, k=int(k/2))
-    # words_compressed = words_compressed.transpose()
+    words_compressed = words_compressed.transpose()
     docs_compressed_normed = normalize(docs_compressed)
+
+    word_to_index = vectorizer.vocabulary_
+    index_to_word = {i:t for t,i in word_to_index.items()}
+
+    categories = {}
+    category_names = [
+    "Creativity & Play",
+    "Artificial Intelligence & Expression",
+    "Science & Environmental",
+    "Identity & Society",
+    "Knowledge & Economy",
+    "Health & Relationships",
+    "Education & Awareness",
+    "Artistic Pursuits",
+    "Childhood & Play",
+    "Cultural & Health Exchange",
+    "Environmental Biology",
+    "Narrative & Nourishment",
+    "Health & Design",
+    "Global Health & Development",
+    "Microbiology & Nutrition",
+    "Health & Science",
+    "Technology & Imagination",
+    "Sustainability & Consumption",
+    "Global Energy & Education",
+    "Marine Ecology & Conservation",
+    "Design & Biology",
+    "Planetary Health & Geology",
+    "Data Science & Diversity",
+    "Language & Planetary Sciences",
+    "Technology & Nutrition",
+    "Psychology & Physiology",
+    "Language & Medicine",
+    "Food Systems & Sociology",
+    "African Art & Health",
+    "Business & Data Analysis",
+    "Water Resources & Linguistics",
+    "Financial Technology & Robotics",
+    "Sleep & Linguistics",
+    "Urban Data Analysis & Climate",
+    "Robotics & Oceanography",
+    "Humor & Environmental Science",
+    "Urban Life & Environmental Studies",
+    "Neuroscience & Environmental Studies",
+    "Data Analysis & Childhood Education",
+    "Cosmology & Health Education",
+    "Brain Studies & Urban Development",
+    "Neurology & Climate Change",
+    "Gender Studies & Energy",
+    "Music Therapy & Environmental Science",
+    "Humor & Earth Sciences",
+    "Music & Oceanography",
+    "Neuroscience & Health",
+    "Psychology & Music Therapy",
+    "Humor & Psychology",
+    "Knowledge & Communication"
+    ]
+
+    for i in range(50):
+        dimension_col = words_compressed[:,i].squeeze()
+        asort = np.argsort(-dimension_col)
+        print([index_to_word[i] for i in asort[:5]])
+        print()
+        categories[i] = category_names[i]
+
+
+    #td_matrix_np = td_matrix.transpose().toarray()
+    #td_matrix_np = normalize(td_matrix_np)
+
+    sim_matrix = docs_compressed_normed.dot(docs_compressed_normed.T)
+    doc_category_scores = docs_compressed_normed.dot(words_compressed.T)
+    return sim_matrix, categories, doc_category_scores
+
 
     # td_matrix_np = td_matrix.transpose().toarray()
     # td_matrix_np = normalize(td_matrix_np)
@@ -54,13 +127,19 @@ def prepare_data():
         idx_to_title[idx] = title
         title_to_idx[title] = idx
 
-    sim_matrix = build_sim_matrix([transcript[1] for transcript in documents])
+    sim_matrix, categories, doc_category_scores = build_sim_matrix([transcript[1] for transcript in documents])
     sim_matrix = np.round(sim_matrix, 4)
 
     with open("idx_to_docnames", 'w') as json_file:
         json.dump(idx_to_title, json_file)
     with open("docname_to_idx", 'w') as json_file:
         json.dump(title_to_idx, json_file)
+
+    with open("categories", "w") as json_file:
+        json.dump(categories, json_file)
+
+    #np.save('doc_category_scores', doc_category_scores)
+
 
     chunk_size = len(sim_matrix) // 6
     chunks = [sim_matrix[i:i+chunk_size]
@@ -69,6 +148,42 @@ def prepare_data():
         filename = f'chunk_{i}.npy'
         np.save(filename, chunk)
 
+    chunks_2 = [doc_category_scores[i:i+chunk_size]
+              for i in range(0, len(doc_category_scores), chunk_size)]
+    for i, chunk in enumerate(chunks):
+        filename = f'dc_chunk_{i}.npy'
+        np.save(filename, chunk)
+
+    
+def get_doc_category_scores(docs, categories):
+    # Load Similarity Matrix
+    loaded_chunks = []
+    for i in range(6):
+        filename = f'chunk_{i}.npy'
+        loaded_chunk = np.load(filename)
+        loaded_chunks.append(loaded_chunk)
+
+    dc_matrix = np.concatenate(loaded_chunks)
+
+    with open("categories", 'r') as json_file:
+        categories_to_idx = json.load(json_file)
+
+    with open("docname_to_idx", 'r') as json_file:
+        docname_to_idx = json.load(json_file)
+
+    idxs = [docname_to_idx[name] for name in docs]
+
+    docs = {}
+    
+    for doc in idxs:
+        docs[doc] = {}
+
+    for c in categories:
+        c_idx = categories_to_idx[c]
+        for doc in idxs:
+            docs[doc][c_idx] = dc_matrix[doc, c_idx]
+
+    return docs
 
 def get_top_10_for_query(query):
     # Load Similarity Matrix
@@ -86,3 +201,5 @@ def get_top_10_for_query(query):
         inv = json.load(json_file)
 
     return get_top_k_talks(query, docname_to_idx, inv, sim_matrix, 10)
+
+#prepare_data()
