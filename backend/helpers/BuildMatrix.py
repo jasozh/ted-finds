2d34f5a9a5193c4ed4ad98d0e36d5a90bbb5a9e3
jackkafif <107105592+jackkafif@users.noreply.github.com>
2024-04-26 22:55:40 -0400

    Removed talks without transcripts (#47)
    * removed talks without transcripts
    
    * Delete main run
    
    ---------
    
    Co-authored-by: jk2298 <jk2298@cornell.edu>
    Co-authored-by: Jason Zheng <jasonz4200@gmail.com>

--- backend/helpers/BuildMatrix.py
+++ backend/helpers/BuildMatrix.py
@@ -42,6 +42,7 @@ def prepare_data():
     with open('../init.json', 'r') as json_file:
         data = json.load(json_file)
         talks = pd.json_normalize(data)
+        talks = talks[talks['transcript'] != '']
 
     # Get transcripts in one place
     documents = []
