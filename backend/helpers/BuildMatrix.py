e386d663eccab7260057da7630f189cbc5828e2e
Jason Zheng <jasonz4200@gmail.com>
2024-04-26 23:02:25 -0400

    Format

--- backend/helpers/BuildMatrix.py
+++ backend/helpers/BuildMatrix.py
@@ -19,75 +19,74 @@ def build_sim_matrix(documents, k=100):
     docs_compressed_normed = normalize(docs_compressed)
 
     word_to_index = vectorizer.vocabulary_
-    index_to_word = {i:t for t,i in word_to_index.items()}
+    index_to_word = {i: t for t, i in word_to_index.items()}
 
     categories = {}
     category_names = [
-    "Creativity & Play",
-    "Artificial Intelligence & Expression",
-    "Science & Environmental",
-    "Identity & Society",
-    "Knowledge & Economy",
-    "Health & Relationships",
-    "Education & Awareness",
-    "Artistic Pursuits",
-    "Childhood & Play",
-    "Cultural & Health Exchange",
-    "Environmental Biology",
-    "Narrative & Nourishment",
-    "Health & Design",
-    "Global Health & Development",
-    "Microbiology & Nutrition",
-    "Health & Science",
-    "Technology & Imagination",
-    "Sustainability & Consumption",
-    "Global Energy & Education",
-    "Marine Ecology & Conservation",
-    "Design & Biology",
-    "Planetary Health & Geology",
-    "Data Science & Diversity",
-    "Language & Planetary Sciences",
-    "Technology & Nutrition",
-    "Psychology & Physiology",
-    "Language & Medicine",
-    "Food Systems & Sociology",
-    "African Art & Health",
-    "Business & Data Analysis",
-    "Water Resources & Linguistics",
-    "Financial Technology & Robotics",
-    "Sleep & Linguistics",
-    "Urban Data Analysis & Climate",
-    "Robotics & Oceanography",
-    "Humor & Environmental Science",
-    "Urban Life & Environmental Studies",
-    "Neuroscience & Environmental Studies",
-    "Data Analysis & Childhood Education",
-    "Cosmology & Health Education",
-    "Brain Studies & Urban Development",
-    "Neurology & Climate Change",
-    "Gender Studies & Energy",
-    "Music Therapy & Environmental Science",
-    "Humor & Earth Sciences",
-    "Music & Oceanography",
-    "Neuroscience & Health",
-    "Psychology & Music Therapy",
-    "Humor & Psychology",
-    "Knowledge & Communication"
+        "Creativity & Play",
+        "Artificial Intelligence & Expression",
+        "Science & Environmental",
+        "Identity & Society",
+        "Knowledge & Economy",
+        "Health & Relationships",
+        "Education & Awareness",
+        "Artistic Pursuits",
+        "Childhood & Play",
+        "Cultural & Health Exchange",
+        "Environmental Biology",
+        "Narrative & Nourishment",
+        "Health & Design",
+        "Global Health & Development",
+        "Microbiology & Nutrition",
+        "Health & Science",
+        "Technology & Imagination",
+        "Sustainability & Consumption",
+        "Global Energy & Education",
+        "Marine Ecology & Conservation",
+        "Design & Biology",
+        "Planetary Health & Geology",
+        "Data Science & Diversity",
+        "Language & Planetary Sciences",
+        "Technology & Nutrition",
+        "Psychology & Physiology",
+        "Language & Medicine",
+        "Food Systems & Sociology",
+        "African Art & Health",
+        "Business & Data Analysis",
+        "Water Resources & Linguistics",
+        "Financial Technology & Robotics",
+        "Sleep & Linguistics",
+        "Urban Data Analysis & Climate",
+        "Robotics & Oceanography",
+        "Humor & Environmental Science",
+        "Urban Life & Environmental Studies",
+        "Neuroscience & Environmental Studies",
+        "Data Analysis & Childhood Education",
+        "Cosmology & Health Education",
+        "Brain Studies & Urban Development",
+        "Neurology & Climate Change",
+        "Gender Studies & Energy",
+        "Music Therapy & Environmental Science",
+        "Humor & Earth Sciences",
+        "Music & Oceanography",
+        "Neuroscience & Health",
+        "Psychology & Music Therapy",
+        "Humor & Psychology",
+        "Knowledge & Communication"
     ]
 
     for i in range(50):
-        dimension_col = words_compressed[:,i].squeeze()
+        dimension_col = words_compressed[:, i].squeeze()
         categories[i] = category_names[i]
 
-
-    #td_matrix_np = td_matrix.transpose().toarray()
-    #td_matrix_np = normalize(td_matrix_np)
+    # td_matrix_np = td_matrix.transpose().toarray()
+    # td_matrix_np = normalize(td_matrix_np)
 
     sim_matrix = docs_compressed_normed.dot(docs_compressed_normed.T)
-    doc_category_scores = docs_compressed#doc_category_scores = docs_compressed_normed.dot(words_compressed.T)
-    #print(sim_matrix.shape, doc_category_scores.shape)
+    # doc_category_scores = docs_compressed_normed.dot(words_compressed.T)
+    doc_category_scores = docs_compressed
+    # print(sim_matrix.shape, doc_category_scores.shape)
     return sim_matrix, categories, doc_category_scores
-
 
     # td_matrix_np = td_matrix.transpose().toarray()
     # td_matrix_np = normalize(td_matrix_np)
@@ -101,6 +100,7 @@ def get_top_k(title, title_to_idx, sim_matrix, k=10):
     top_k_indices = sim_matrix[idx].argsort()[::-1][1:k]
     top_k_values = sim_matrix[idx][top_k_indices]
     return top_k_indices, top_k_values
+
 
 def get_doc_category_scores(docs, categories, dc_matrix, docname_to_idx, idx_to_categories):
 
@@ -117,18 +117,21 @@ def get_doc_category_scores(docs, categories, dc_matrix, docname_to_idx, idx_to_
 
     return docs
 
+
 def get_categories(query, dc_matrix, names_to_idx):
     idx = names_to_idx[query]
     x = np.argsort(dc_matrix[idx])[:10]
     return np.argsort(dc_matrix[idx])[:10]
 
+
 def get_top_k_talks(title, title_to_idx, idx_to_title, sim_matrix, dc_matrix, idx_to_categories, categories_to_idx, k=10):
     top_k_indices, top_k_values = get_top_k(title, title_to_idx, sim_matrix, k)
-    #tops =  [(idx_to_title[str(idx)], score) for (idx, score) in zip(top_k_indices, top_k_values)]
+    # tops =  [(idx_to_title[str(idx)], score) for (idx, score) in zip(top_k_indices, top_k_values)]
     docs = [idx_to_title[str(idx)] for idx in top_k_indices]
     categories = get_categories(title, dc_matrix, title_to_idx)
-    top_dc_scores = get_doc_category_scores(docs, categories, dc_matrix, title_to_idx, idx_to_categories)
-    #query_scores = [{idx_to_categories[str(cat)] : dc_matrix[title_to_idx[title], cat]} for cat in categories]
+    top_dc_scores = get_doc_category_scores(
+        docs, categories, dc_matrix, title_to_idx, idx_to_categories)
+    # query_scores = [{idx_to_categories[str(cat)] : dc_matrix[title_to_idx[title], cat]} for cat in categories]
     query_scores = {}
     for cat in categories:
         category = idx_to_categories[str(cat)]
@@ -153,7 +156,8 @@ def prepare_data():
         idx_to_title[idx] = title
         title_to_idx[title] = idx
 
-    sim_matrix, categories, doc_category_scores = build_sim_matrix([transcript[1] for transcript in documents])
+    sim_matrix, categories, doc_category_scores = build_sim_matrix(
+        [transcript[1] for transcript in documents])
     sim_matrix = np.round(sim_matrix, 4)
 
     with open("idx_to_docnames", 'w') as json_file:
@@ -169,9 +173,7 @@ def prepare_data():
     with open("categories_inv", "w") as json_file:
         json.dump(categories_inv, json_file)
 
-
-    #np.save('doc_category_scores', doc_category_scores)
-
+    # np.save('doc_category_scores', doc_category_scores)
 
     chunk_size = len(sim_matrix) // 6
     chunks = [sim_matrix[i:i+chunk_size]
@@ -181,13 +183,13 @@ def prepare_data():
         np.save(filename, chunk)
 
     chunks_2 = [doc_category_scores[i:i+chunk_size]
-              for i in range(0, len(doc_category_scores), chunk_size)]
+                for i in range(0, len(doc_category_scores), chunk_size)]
     for i, chunk in enumerate(chunks_2):
         filename = f'dc_chunk_{i}.npy'
         np.save(filename, chunk)
 
-#prepare_data()
-    
+# prepare_data()
+
 
 def get_top_10_for_query(query):
     # Load Similarity Matrix
@@ -221,8 +223,9 @@ def get_top_10_for_query(query):
     dc_scores = get_doc_category_scores(titles, categories)
 
     zipped = zip(top_k_talks, dc_scores)
-    
+
     return zipped
+
 
 if __name__ == '__main__':
     prepare_data()
